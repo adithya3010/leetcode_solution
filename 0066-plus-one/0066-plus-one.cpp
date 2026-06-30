d95872893f7554f4366188067043e1ef6cfe23a5
adithya3010 <adithyamotapallukula@gmail.com>
2026-06-30 14:13:00 +0530

    Time: 0 ms (100%), Space: 11.6 MB (27.06%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        if(digits[n-1]!=9){
+            digits[n-1]=digits[n-1]+1;
+            return digits;
+            }else{
+                int nonNine=-1;
+                for(int i=n-1;i>=0;i--){
+                    if(digits[i]!=9){
+                        nonNine=i;
+                        break;
+                    }
+                }
+                if(nonNine==-1){
+                    digits[nonNine+1]=1;
+                    for(int i=nonNine+2;i<digits.size();i++){
+                        digits[i]=0;
+                    }
+                    digits.push_back(0);
+                }else{
+                digits[nonNine]++;
+                for(int i=nonNine+1;i<digits.size();i++){
+                    digits[i]=0;
+                }
+            }
+                }
+       return digits;
+            }
+
+    };
