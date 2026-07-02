f056ccb4242b54b0f814e8506bebb154326dd079
adithya3010 <adithyamotapallukula@gmail.com>
2026-07-02 15:47:55 +0530

    Time: 0 ms (100%), Space: 23.7 MB (63.66%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         if(nums.size()==0) return {};
+        vector <int> ans;
         int n=nums.size();
-        unordered_map <int,int> mp;
-        vector<int> ans;
-        for(int i=0;i<n;i++)mp[nums[i]]++;
-        for(auto it:mp){
-            if(it.second>(n/3))ans.push_back(it.first);
-            if(ans.size()==2)break;
+        int cnt1=0,cnt2=0;
+        int el1,el2;
+        for(int i=0;i<n;i++){
+            if(cnt1==0&&el2!=nums[i]){
+                el1=nums[i];
+                cnt1=1;
+            }else if(cnt2==0&&el1!=nums[i]){
+                el2=nums[i];
+                cnt2=1;
+            }else if(nums[i]==el1){
+                cnt1++;
+            }else if(nums[i]==el2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
         }
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1&&el1!=el2)cnt1++;
+            if(nums[i]==el2)cnt2++;           
+        }
+        if(cnt1>(n/3))ans.push_back(el1);
+        if(cnt2>(n/3))ans.push_back(el2);
         return ans;
     }
 };
